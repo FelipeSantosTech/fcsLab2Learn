90c1c9d2a3a33f3265018a925a456133d0d5cab6
FelipeSantosTech <felpsantos.c@gmail.com>
2026-03-02 17:28:43 -0300

    Initial Commit

---
+++ fcsLab2Learn/fcsLab2LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  fcsLab2LearnApp.swift
+//  fcsLab2Learn
+//
+//  Created by Paulo Marcelo Santos on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct fcsLab2LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
