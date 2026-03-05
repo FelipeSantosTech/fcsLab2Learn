25d34d9b7b082df03579ac4b6a77f24716fc223e
FelipeSantosTech <felpsantos.c@gmail.com>
2026-03-05 19:43:16 -0300

    created AnswersView file

---
+++ fcsLab2Learn/AnswersView.swift
@@ -0,0 +1,22 @@
+//
+//  AnswersView.swift
+//  fcsLab2Learn
+//
+//  Created by Paulo Marcelo Santos on 05/03/26.
+//
+
+import SwiftUI
+
+struct AnswersView: View {
+    var body: some View {
+        ScrollView {
+            Text("QUESTIONS I HAD")
+                .bold()
+                .font(.title)
+        }
+    }
+}
+
+#Preview {
+    AnswersView()
+}
